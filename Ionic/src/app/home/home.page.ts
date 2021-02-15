0280837699e11b4fee3ca4415657bd8b2159b938
Antonio Romero <a.romero@docline.es>
2021-02-15 10:17:50 +0100

    Added Ionic example project

---
+++ Ionic/src/app/home/home.page.ts
@@ -0,0 +1,71 @@
+import { Component } from '@angular/core';
+import docline from 'cordova-plugin-docline-sdk';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  code: string = "";
+
+  constructor() {}
+
+  /**
+   * join handle action
+   */
+  join() {
+    this.configureEventAndError();
+    docline.join(this.code);
+  }
+
+  /**
+   * Docline Logic
+   */
+
+  configureEventAndError() {
+    let eventId = docline.EventId.consultationJoinSuccess;
+    let eventId2 = docline.EventId.updatedCameraStatus;
+    
+    docline.addEventListener(eventId, this.consultationJoinSuccess);
+    docline.addEventListener(eventId2, this.updatedCameraStatus);
+    docline.setHandleError(this.handleError);
+  }
+
+  handleError(error) {
+    let types = docline.ErrorType;
+
+    switch (error.type) {
+      case types.unauthorizedError:
+        console.log("unauthorizedError");
+        alert("unauthorizedError");
+        break;
+      case types.emptyCodeError:
+        console.log("emptyCodeError");
+        alert("emptyCodeError");
+        break;
+      case types.connectionError:
+        console.log("connectionError");
+        alert("connectionError");
+        break;
+      case types.customError:
+        console.log(`customError(${error.message})`);
+        alert("customError");
+        break;
+      case types.defaultError:
+        console.log("defaultError");
+        alert("defaultError");
+        break;
+      default: 
+        break;
+    }    
+  }
+
+  consultationJoinSuccess() {
+    console.log(`consultationJoinSuccess`);
+  }
+
+  updatedCameraStatus(data) {
+    console.log(`updatedCameraState: { eventId: ${data.eventId}, screenId: ${data.screenId}, isEnabled: ${data.isEnabled} }`);
+  }
+}
